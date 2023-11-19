0a3e52f846d9e85bf5113256cc225ac4d711a108
Julian Dworzycki <mr.jujek@gmail.com>
2023-11-19 22:05:03 +0100

    :hammer: Make bud a function

--- oi-31/bud/bud.cpp
+++ oi-31/bud/bud.cpp
@@ -49,15 +49,138 @@ bool check_no_common_points(std::vector<std::vector<position>> arr)
     return false;
 }
 
+int bud(int n, int m, int **airport)
+{
+    vector<vector<position>> posibilities[n] = {};
+
+    vector<position> temp = {};
+    if (m == 1)
+    {
+        int longest = 0;
+
+        // Horizontal
+        for (int i = 0; i < n; i++)
+        {
+            temp.clear();
+            for (int j = 0; j < n; j++)
+            {
+                if (airport[i][j] == 0)
+                {
+                    temp.push_back({j, i, airport[i][j]});
+                }
+                else
+                {
+                    if (temp.size() > longest)
+                    {
+                        longest = temp.size();
+                    }
+                    temp.clear();
+                }
+            }
+
+            if (temp.size() > longest)
+            {
+                longest = temp.size();
+            }
+        }
+
+        // Vertical
+        for (int i = 0; i < n; i++)
+        {
+            temp.clear();
+            for (int j = 0; j < n; j++)
+            {
+                if (airport[j][i] == 0)
+                {
+                    temp.push_back({i, j, airport[j][i]});
+                }
+                else
+                {
+                    if (temp.size() > longest)
+                    {
+                        longest = temp.size();
+                    }
+                    temp.clear();
+                }
+            }
+
+            if (temp.size() > longest)
+            {
+                longest = temp.size();
+            }
+        }
+
+        return longest;
+    }
+    else
+    {
+        // Horizontal
+        for (int i = 0; i < n; i++)
+        {
+            temp.clear();
+            for (int j = 0; j < n; j++)
+            {
+                if (airport[i][j] == 0)
+                {
+                    temp.push_back({j, i, airport[i][j]});
+                }
+                else if (temp.size() > 0)
+                {
+                    posibilities[temp.size() - 1].push_back(temp);
+                    temp.clear();
+                }
+            }
+            if (temp.size() > 0)
+            {
+                posibilities[temp.size() - 1].push_back(temp);
+            }
+        }
+
+        // Vertical
+        for (int i = 0; i < n; i++)
+        {
+            temp.clear();
+            for (int j = 0; j < n; j++)
+            {
+                if (airport[j][i] == 0)
+                {
+                    temp.push_back({i, j, airport[j][i]});
+                }
+                else if (temp.size() > 0)
+                {
+                    posibilities[temp.size() - 1].push_back(temp);
+                    temp.clear();
+                }
+            }
+            if (temp.size() > 0)
+            {
+                posibilities[temp.size() - 1].push_back(temp);
+            }
+        }
+    }
+    
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (posibilities[i].size() > 1)
+        {
+            if (check_no_common_points(posibilities[i]))
+            {
+                return i + 1;
+            }
+        }
+    }
+}
+
 int main()
 {
     int n, m;
     cin >> n >> m;
-    int airport[n][n];
+    int *airport[n];
 
     char area;
     for (int i = 0; i < n; i++)
     {
+        airport[i] = new int[n];
         for (int j = 0; j < n; j++)
         {
             cin >> area;
@@ -72,171 +195,7 @@ int main()
         }
     }
 
-    cout << "---- END OF INPUT ----\n";
+    cout << bud(n, m, airport);
 
-    vector<vector<position>> posibilities[n] = {};
-
-    vector<position> temp = {};
-    if (m == 1)
-    {
-        cout << "---- M == 1 ----\n";
-
-        int longest = 0;
-
-        // Horizontal
-        for (int i = 0; i < n; i++)
-        {
-            temp.clear();
-            for (int j = 0; j < n; j++)
-            {
-                // if (temp.size() == n)
-                // {
-                //     cout << "1RETURNING: " << temp.size();
-                //     return temp.size();
-                // }
-
-                if (airport[i][j] == 0)
-                {
-                    temp.push_back({j, i, airport[i][j]});
-                }
-                else
-                {
-                    if (temp.size() > longest)
-                    {
-                        longest = temp.size();
-                    }
-                    temp.clear();
-                }
-            }
-
-            if (temp.size() > longest)
-            {
-                longest = temp.size();
-            }
-        }
-
-        // Vertical
-        for (int i = 0; i < n; i++)
-        {
-            temp.clear();
-            for (int j = 0; j < n; j++)
-            {
-                // if (temp.size() == n)
-                // {
-                //     cout << "2RETURNING: " << temp.size();
-                //     return temp.size();
-                // }
-
-                if (airport[j][i] == 0)
-                {
-                    temp.push_back({i, j, airport[j][i]});
-                }
-                else
-                {
-                    if (temp.size() > longest)
-                    {
-                        longest = temp.size();
-                    }
-                    temp.clear();
-                }
-            }
-
-            if (temp.size() > longest)
-            {
-                longest = temp.size();
-            }
-        }
-
-        cout << "RETURNING: longest = " << longest;
-        return longest;
-    }
-    else
-    {
-        cout << "---- M != 1 ----\n";
-
-        // Horizontal
-        for (int i = 0; i < n; i++)
-        {
-            temp.clear();
-            for (int j = 0; j < n; j++)
-            {
-                if (airport[i][j] == 0)
-                {
-                    temp.push_back({j, i, airport[i][j]});
-                }
-                else if (temp.size() > 0)
-                {
-                    posibilities[temp.size() - 1].push_back(temp);
-                    temp.clear();
-                }
-            }
-            if (temp.size() > 0)
-            {
-                posibilities[temp.size() - 1].push_back(temp);
-            }
-        }
-
-        // Vertical
-        for (int i = 0; i < n; i++)
-        {
-            temp.clear();
-            for (int j = 0; j < n; j++)
-            {
-                if (airport[j][i] == 0)
-                {
-                    temp.push_back({i, j, airport[j][i]});
-                }
-                else if (temp.size() > 0)
-                {
-                    posibilities[temp.size() - 1].push_back(temp);
-                    temp.clear();
-                }
-            }
-            if (temp.size() > 0)
-            {
-                posibilities[temp.size() - 1].push_back(temp);
-            }
-        }
-    }
-
-    cout << "\n---- POSIBILITIES ----\n";
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Size: " << i + 1 << endl;
-        for (int j = 0; j < posibilities[i].size(); j++)
-        {
-            cout << "\tPosibility: " << j << endl;
-            for (int k = 0; k < posibilities[i][j].size(); k++)
-            {
-                cout << "\t" << posibilities[i][j][k].x << " " << posibilities[i][j][k].y << " " << posibilities[i][j][k].value << endl;
-            }
-        }
-    }
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        if (posibilities[i].size() > 1)
-        {
-            cout << "i: " << i + 1 << ", posibilities[i].size(): " << posibilities[i].size() << endl;
-
-            std::cout << (check_no_common_points(posibilities[i]) ? "True" : "False") << std::endl;
-
-            if (check_no_common_points(posibilities[i]))
-            {
-                cout << "RETURNING: " << i + 1;
-                return i + 1;
-            }
-        }
-    }
-
-    cout << "\n---- AIRPORT ----\n";
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << airport[i][j] << " ";
-        }
-        cout << endl;
-    }
     return 0;
 }
