27a48e061bdb460577e9ada6cd19c4a6d2584f7d
Julian Dworzycki <mr.jujek@gmail.com>
2023-11-19 19:32:17 +0100

    :rotating_light: Save errors to file

--- oi-31/cza/cza.cpp
+++ oi-31/cza/cza.cpp
@@ -18,10 +18,14 @@ std::string ChatBBB(long long k, long long a, long long b, std::string S)
 {
     long long n = S.size();
 
+    std::cout << "N: " << n << std::endl;
+    std::cout << "b: " << b << std::endl;
+
     if (n == b)
         return S.substr(a - 1, b);
 
     std::string suffix = S.substr(n - k, n);
+    std::cout << "Suffix: " << suffix << std::endl;
 
     long long pos = 0;
     std::array<long long, 128> arr;
@@ -50,6 +54,9 @@ std::string ChatBBB(long long k, long long a, long long b, std::string S)
     }
 
     S += most_common;
+
+    std::cout << "Most common: " << most_common << std::endl;
+
     return ChatBBB(k, a, b, S);
 }
 
@@ -71,73 +78,86 @@ int main()
     long long N, k, a, b;
     std::string S;
 
-    int test_id;
-    // std::cin >> test_id;
-    // for (int i = test_id * 1000 - 1000 + 1; i <= test_id * 1000; i++)
-    for (int i = 3000 + 1; i <= 4000; i++)
-    {
-        std::cout << "Test ID: " << i << std::endl;
+    // int test_id;
+    // // std::cin >> test_id;
+    // // for (int i = test_id * 1000 - 1000 + 1; i <= test_id * 1000; i++)
+    // std::fstream input;
+    // std::fstream output;
 
-        std::fstream input;
-        input.open("../../oi_2023_testy/cza/in/" + std::to_string(i) + ".in", std::ios::in);
+    // std::fstream errors_file;
+    // errors_file.open("errors.txt", std::ios::out);
 
-        std::fstream output;
-        output.open("../../oi_2023_testy/cza/out/" + std::to_string(i) + ".out", std::ios::in);
+    // for (int i = 3000 + 1; i <= 4000; i++)
+    // {
+    //     std::cout << "Test ID: " << i << std::endl;
 
-        if (!input.is_open() || !output.is_open())
-        {
-            i++;
-            std::cout << "File not found: " << i << std::endl;
-            continue;
-        }
+    //     input.open("../../oi_2023_testy/cza/in/" + std::to_string(i) + ".in", std::ios::in);
 
-        input >> N >> k >> a >> b >> S;
+    //     output.open("../../oi_2023_testy/cza/out/" + std::to_string(i) + ".out", std::ios::in);
 
-        // std::cout << "N: " << N << std::endl;
-        // std::cout << "k: " << k << std::endl;
-        // std::cout << "a: " << a << std::endl;
-        // std::cout << "b: " << b << std::endl;
-        // std::cout << "S: " << S << std::endl;
+    //     if (!input.is_open() || !output.is_open())
+    //     {
+    //         i++;
+    //         std::cout << "File not found: " << i << std::endl;
+    //         continue;
+    //     }
 
-        std::string expected;
-        output >> expected;
+    //     input >> N >> k >> a >> b >> S;
 
-        // std::cout << "Expected: " << expected << std::endl;
+    //     // std::cout << "N: " << N << std::endl;
+    //     // std::cout << "k: " << k << std::endl;
+    //     // std::cout << "a: " << a << std::endl;
+    //     // std::cout << "b: " << b << std::endl;
+    //     // std::cout << "S: " << S << std::endl;
 
-        std::string actual = ChatBBB(k, a, b, S);
+    //     std::string expected;
+    //     output >> expected;
 
-        // std::cout << "Actual: " << actual << std::endl;
+    //     // std::cout << "Expected: " << expected << std::endl;
 
-        if (expected != actual)
-        {
-            error_list.push_back({i, expected, actual});
-        }
+    //     std::string actual = ChatBBB(k, a, b, S);
 
-        input.close();
-        output.close();
-    }
+    //     // std::cout << "Actual: " << actual << std::endl;
 
-    if (error_list.size() == 0)
-    {
-        std::cout << "OK" << std::endl;
-    }
-    else
-    {
-        std::cout << "Errors: " << std::endl;
-        for (auto &error : error_list)
-        {
-            std::cout << "Test ID: " << error.test_id << std::endl;
-            std::cout << "Expected: " << error.expected << std::endl;
-            std::cout << "Actual: " << error.actual << std::endl
-                      << std::endl;
-        }
+    //     if (expected != actual)
+    //     {
+    //         error_list.push_back({i, expected, actual});
 
-        std::cout << "Errors number: " << error_list.size() << std::endl;
-    }
+    //         errors_file << "Test ID: " << i << std::endl;
+    //         errors_file << "Expected: " << expected << std::endl;
+    //         errors_file << "Actual: " << actual << std::endl
+    //                     << std::endl;
+    //     }
 
-    // std::cin >> N >> k >> a >> b >> S;
+    //     input.close();
+    //     output.close();
+    // }
 
-    // std::cout << ChatBBB(k, a, b, S) << std::endl;
+    // errors_file.close();
+
+    // if (error_list.size() == 0)
+    // {
+    //     std::cout << "OK" << std::endl;
+    // }
+    // else
+    // {
+    //     std::cout << "Errors: " << std::endl;
+    //     for (auto &error : error_list)
+    //     {
+    //         std::cout << "Test ID: " << error.test_id << std::endl;
+    //         std::cout << "Expected: " << error.expected << std::endl;
+    //         std::cout << "Actual: " << error.actual << std::endl
+    //                   << std::endl;
+    //     }
+
+    //     std::cout << "Errors number: " << error_list.size() << std::endl;
+    // }
+
+    std::cin >> N >> k >> a >> b >> S;
+
+    std::cout << N << k << a << b << S << std::endl;
+
+    std::cout << ChatBBB(k, a, b, S) << std::endl;
 
     return 0;
 }
