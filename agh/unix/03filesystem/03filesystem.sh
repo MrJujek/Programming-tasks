2333530a6a0e152ae6cc332512250d37b1b004f9
Julian Dworzycki <mr.jujek@gmail.com>
2024-11-17 17:44:49 +0100

    ✨ UNIX 03

--- agh/unix/03filesystem/03filesystem.sh
+++ agh/unix/03filesystem/03filesystem.sh
@@ -14,3 +14,29 @@ umask 0007
 #7
 find ~ -type f -exec du -h {} + | sort -rh | head -n 1
 find ~ -maxdepth 2 -type f -exec du -h {} + | sort -rh | head -n 1
+#8
+find ~ -maxdepth 2 -type f -exec stat --format "%b %n" {} + | sort -nr | head -n 1
+#9
+find /usr/include -type f -name "c*" -exec ls -lh {} +
+#10
+find ~ -type f -mtime +2 -exec ls -lh {} +
+find ~ -maxdepth 1 -type f -mtime +2 -exec ls -lh {} +
+#11
+find /usr/include -maxdepth 1 -type f -name "m*.h" -size -12k | wc -l
+#12
+tar -cvzf c2.tar.gz c2/
+rm -r c2
+tar -xvzf c2.tar.gz
+#13
+du -sh ~
+#14
+df -h
+#15
+lsblk -f
+#16
+du -sh /usr/* | sort -rh
+#17
+ln -s $HOME/c2/bin/moj_ls $HOME/c2/text/symbol
+ln $HOME/c2/text/ls.txt $HOME/c2/bin/twardy
+ls -lRi $HOME/c2
+
